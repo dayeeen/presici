71c483496a7cf017c82e28f81d1ba969d868d4e1
dayen <nubicodes@gmail.com>
2023-05-31 13:54:12 +0700

    Catatan hanya diakses oleh siswa yg sakit/izin

--- view/add_note.php
+++ view/add_note.php
@@ -1,3 +1,25 @@
+<?php 
+// Jika sudah absen, maka tidak bisa menambahkan catatan
+$this_day = date("d");
+$sql = "SELECT * FROM data_absen NATURAL LEFT JOIN tanggal WHERE nama_tgl='$this_day' AND id_user='$_SESSION[id]'";
+$query_tday = $conn->query($sql);
+
+if ($query_tday->num_rows > 0) {
+    $row = $query_tday->fetch_assoc();
+    if ($row['st_ab_lain'] == 'Menunggu') {
+        echo "<div class='alert alert-warning'><strong>Tambahkan catatan jika anda sakit atau izin.</strong></div>";
+        $disable = "";
+    } else {
+        echo "<div class='alert alert-warning'><strong>Anda sudah absen hari ini. Tidak bisa menambahkan catatan.</strong></div>";
+        $disable = "disabled='disabled'";
+    }
+} else {
+    echo "<div class='alert alert-warning'><strong>Anda belum absen hari ini. Tidak bisa menambahkan catatan.</strong></div>";
+    $disable = "disabled='disabled'";
+}
+
+
+?>
 <label><h3>Tambah Catatan</h3></label>
 <form method="post" action="./model/proses.php" name="form1" id="form1" onSubmit="return valregister()">
 	<div class="table-responsive">
@@ -9,7 +31,7 @@
 	   </tr>
 	   <tr>
 	   		<td style="border-top:none;">
-	   			<button type="submit" name="simpan_note" id="save" onclick="saveForm(); return false;" class="btn btn-success">Simpan</button>
+	   			<button type="submit" name="simpan_note" id="save" onclick="saveForm(); return false;" class="btn btn-success" <?php echo $disable ?>>Simpan</button>
 	   		</td>
 	   </tr>
 	  </table>
@@ -21,7 +43,7 @@ function valregister(){
                         alert("Catatan tidak boleh kosong");
                         form1.note.focus();
                         return false;
-            } 
+            }
              return true; 
 }
 </script>
