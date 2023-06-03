09ef1622a79c372984b3ca0c593bf3ca17f3ade7
dayen <nubicodes@gmail.com>
2023-06-03 17:30:51 +0700

    navbar

--- model/layout/navbar.php
+++ model/layout/navbar.php
@@ -1,4 +1,3 @@
-<link rel="stylesheet" href="./././lib/navbar.css">
 <div class="navbar-header">
 	<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
 		aria-expanded="false" aria-controls="navbar"> <span class="sr-only">Toggle navigation</span>
@@ -6,28 +5,28 @@
 		<span class="icon-bar"></span>
 		<span class="icon-bar"></span>
 	</button>
-	<a class="navbar-brand" href="home">Presensi SMAN 1 Cibaduyut</a>
+	<a class="navbar-brand" href="home" style="color: #fff">Presensi SMAN 1 Cibaduyut</a>
 </div>
 <div id="navbar" class="navbar-collapse collapse">
 	<ul class="nav navbar-nav navbar-right visible-xs visible-sm">
-		<li id="output_m"></li>
-		<?php
-		if (isset($_SESSION['pb'])) {
-			$link = array("", "add_siswa", "siswa", "absen", "absensi", "req_catatan", "catatan", "katasandi&id=$_SESSION[id]", "keluar");
-			$name = array("", "Tambah Siswa", "Daftar Siswa", "Absen", "Lihat Absensi", "Catatan", "Lihat Catatan", "Ubah Katasandi", "Keluar");
+		<li id="output_m">
+			<?php
+			if (isset($_SESSION['pb'])) {
+				$link = array("", "add_siswa", "siswa", "absen", "absensi", "req_catatan", "catatan", "katasandi&id=$_SESSION[id]", "keluar");
+				$name = array("", "Tambah Siswa", "Daftar Siswa", "Absen", "Lihat Absensi", "Catatan", "Lihat Catatan", "Ubah Katasandi", "Keluar");
 
-			for ($i = 1; $i <= count($link) - 1; $i++) {
-				echo "<li><a href='$link[$i]'>$name[$i]</a></li>";
+				for ($i = 1; $i <= count($link) - 1; $i++) {
+					echo "<li><a href='$link[$i]'>$name[$i]</a></li>";
+				}
+			} elseif (isset($_SESSION['sw'])) {
+				$link = array("", "absen", "absensi", "tambah_catatan", "catatan", "keluar");
+				$name = array("", "Absen", "Laporan Absensi", "Tambah Catatan", "Catatan", "Keluar");
+
+				for ($i = 1; $i <= count($link) - 1; $i++) {
+					echo "<li><a href='$link[$i]'>$name[$i]</a></li>";
+				}
 			}
-		} elseif (isset($_SESSION['sw'])) {
-			$link = array("", "absen", "absensi", "tambah_catatan", "catatan", "keluar");
-			$name = array("", "Absen", "Laporan Absensi", "Tambah Catatan", "Catatan", "Keluar");
-
-			for ($i = 1; $i <= count($link) - 1; $i++) {
-
-				echo "<li><a href='$link[$i]'>$name[$i]</a></li>";
-			}
-		}
-		?>
+			?>
+		</li>
 	</ul>
 </div>
