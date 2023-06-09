6d2ff64b008357db8cc932c8756b791f6857d67c
dayen <nubicodes@gmail.com>
2023-06-09 22:57:33 +0700

    fix tampilan waktu

--- view/media.php
+++ view/media.php
@@ -1,77 +1,88 @@
 <!DOCTYPE html>
-<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="./lib/img/logo.png">
+<html lang="en">
 
-    <title>Presici</title>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="./lib/img/logo.png">
 
-    <link href="./lib/bootstrap.min.css" rel="stylesheet">
-    <link href="./lib/ie10-viewport-bug-workaround.css" rel="stylesheet">
+  <title>Presici</title>
 
-    <link href="./lib/dashboard.css" rel="stylesheet">
-    <script src="./lib/ie-emulation-modes-warning.js"></script>
+  <link href="./lib/bootstrap.min.css" rel="stylesheet">
+  <link href="./lib/ie10-viewport-bug-workaround.css" rel="stylesheet">
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
+  <link href="./lib/dashboard.css" rel="stylesheet">
+  <script src="./lib/ie-emulation-modes-warning.js"></script>
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <script type="text/javascript">
-    window.setTimeout("waktu()",1000);    
-    function waktu() {     
-    var tanggal = new Date();    
-    setTimeout("waktu()",1000);    
-    document.getElementById("output").innerHTML = tanggal.getHours()+":"+tanggal.getMinutes()+" WIB";  
-    }
-    // window.setTimeout("waktu_m()",1000);    
-    // function waktu_m() {     
-    // var tanggal = new Date();    
-    // setTimeout("waktu_m()",1000);    
-    // document.getElementById("output_m").innerHTML = tanggal.getHours()+":"+tanggal.getMinutes()+" WIB";  
-    // }   
-</script>
-  </head>
+  <script type="text/javascript">
+    window.setTimeout("waktu()", 0);
+    function waktu() {
+      var tanggal = new Date();
+      setTimeout("waktu()", 0);
+      if (tanggal.getHours() < 10) {
+        $jam = "0" + tanggal.getHours();
+      } else {
+        $jam = tanggal.getHours();
+      }
+      if (tanggal.getMinutes() < 10) {
+        $menit = "0" + tanggal.getMinutes();
+      } else {
+        $menit = tanggal.getMinutes();
+      }
+      document.getElementById("output").innerHTML = $jam + ":" + $menit + " WIB";
+      setTimeout("waktu()", 0);
+      document.getElementById("output").innerHTML = tanggal.getHours() + ":" + tanggal.getMinutes() + " WIB";
+    }  
+  </script>
+</head>
 
-  <body>
+<body>
 
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container-fluid">
-        <?php 
-        include './model/layout/navbar.php';
+  <nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container-fluid">
+      <?php
+      include './model/layout/navbar.php';
+      ?>
+    </div>
+  </nav>
+
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-sm-3 col-md-2 sidebar">
+        <?php
+        include './model/layout/sidebar.php';
         ?>
       </div>
-    </nav>
-
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-sm-3 col-md-2 sidebar">
-          <?php 
-          include './model/layout/sidebar.php';
-          ?>  
-        </div>
-        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-          <?php 
-          include './model/layout/content.php';
-          ?>
-        </div>
+      <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+        <?php
+        include './model/layout/content.php';
+        ?>
       </div>
     </div>
+  </div>
 
   <!-- Bootstrap core JavaScript
     ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="./lib/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
-    <script src="./lib/bootstrap.min.js"></script>
-    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
-    <script src="./lib/holder.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="./lib/ie10-viewport-bug-workaround.js"></script>
-  
+  <!-- Placed at the end of the document so the pages load faster -->
+  <script src="./lib/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+  <script src="./lib/bootstrap.min.js"></script>
+  <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+  <script src="./lib/holder.min.js"></script>
+  <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+  <script src="./lib/ie10-viewport-bug-workaround.js"></script>
 
-</body></html>
+
+</body>
+
+</html>
